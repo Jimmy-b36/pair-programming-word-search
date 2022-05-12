347009fd128cb4c767e4da97835ec80ee63111fe
James Ball <jamie.j.ball@gmail.com>
2022-05-11 18:11:50 -0700

    function to search array for words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = [];
 
-module.exports = wordSearch
+  for (let i = 0; i < horizontalJoin[0].length; i++) {
+    const verticalArray = [];
+    for (let j = 0; j < horizontalJoin.length; j++) {
+      verticalArray.push(horizontalJoin[j][i]);
+    }
+    verticalJoin.push(verticalArray.join(""));
+  }
+
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
